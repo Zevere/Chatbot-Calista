a3f062e05dc79a5f7c13a6374fde2e76b34778d3
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-09-12 14:07:15 -0400

    fix routing bug

--- src/task-list/task-list.router.js
+++ src/task-list/task-list.router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { foo, bar } from './task-list.controller';
 const taskListRouter = express.Router();
 
-''  |> (route => taskListRouter.get(route, foo))
-    |> (route => taskListRouter.post(route, bar));
+''  |> (route => taskListRouter.get(route, foo));
+''  |> (route => taskListRouter.post(route, bar));
 
 module.exports = taskListRouter;
