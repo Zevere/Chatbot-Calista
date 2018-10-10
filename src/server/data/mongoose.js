34b2fb4092ec46ff90a7148722352d599fd62a9e
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-10-10 17:54:17 -0400

    try example at https://mongoosejs.com/docs/schematypes.html

--- src/server/data/mongoose.js
+++ src/server/data/mongoose.js
@@ -2,13 +2,17 @@ import * as mongoose from 'mongoose';
 import Winston from '../../logging/app.logger';
 
 async function dbconnection(): mongoose.Connection {
-    let db = await mongoose.connect(process.env.DB_CONNECTION_STRING);
-    db.connection.on('error', console.error.bind(console, 'connection error: '));
-    db.connection.once('open', () => {
-        Winston.info('Connected to database.');
-    });
-    
-    return db.connection;
+    let db;
+    try {
+        db = await mongoose.connect(process.env.DB_CONNECTION_STRING);
+        db.connection.on('error', console.error.bind(console, 'connection error: '));
+        db.connection.once('open', () => {
+            Winston.info('Connected to database.');
+        });
+        return db.connection;
+    } catch (exception) {
+        
+    }
 }
 
 
