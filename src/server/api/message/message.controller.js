a841b1ff6ca30fdd491e15b964e01ab481fffed8
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-10-10 20:21:55 -0400

    use connection

--- src/server/api/message/message.controller.js
+++ src/server/api/message/message.controller.js
@@ -5,14 +5,16 @@ import Winston from '../../../logging/app.logger';
 import { WebClient } from '@slack/client';
 import { prettyJson } from '../../../logging/format';
 import dbconnection from '../../data/mongoose';
-import { Schema, Model, model } from 'mongoose';
+import mongoose from 'mongoose';
 
 export async function messageGeneralChat(req: Request, res: Response, next: NextFunction) {
     res.status(200).send('Got it!'); // basic receipt: https://api.slack.com/slash-commands?#responding_basic_receipt
     req.body |> prettyJson |> Winston.info;
-    const schema = new Schema({ any: Schema.Types.Mixed });
-    const M = model('message', schema);
-    const requestBodyModel = new M;
+
+    let conn = dbconnection();
+    const schema = new mongoose.Schema({ any: Schema.Types.Mixed });
+    const M = conn.model('message', schema);
+    const requestBodyModel = new M();
     requestBodyModel.mixed = req.body;
     requestBodyModel.save((err, _) => {
         err |> prettyJson |> Winston.error;
