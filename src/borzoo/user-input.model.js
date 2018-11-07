59e9bf39bdf0f69240371258e73bbe5c5f1afde9
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-07 18:55:46 -0500

    add user input model

---
+++ src/borzoo/user-input.model.js
@@ -0,0 +1,52 @@
+// @flow
+export class UserInput {
+    /**
+     * The desired user name. User names are case-insensitive. 
+     * Valid characters are ASCII alphanumeric characters, '_', '.', and '-'.
+     * @type {string}
+     * @memberof UserInput
+     */
+    name: string;
+
+    /**
+     * User's first name.
+     * @type {string}
+     * @memberof UserInput
+     */
+    firstName: string;
+
+    /**
+     * Passphrase in clear text.
+     * @type {string}
+     * @memberof UserInput
+     */
+    passPhrase: string;
+
+    /**
+     * User's last name.
+     * 
+     * @optional
+     * @type {string}
+     * @memberof UserInput
+     */
+    lastName: string;
+
+    /**
+     *
+     * If creating an organization user, user IDs of team members. 
+     * They will be added as collaborators.
+     * 
+     * @optional
+     * @type {string[]}
+     * @memberof UserInput
+     */
+    members: string[];
+
+    constructor() {
+        this.name = '';
+        this.firstName = '';
+        this.passPhrase = '';
+        this.lastName = '';
+        this.members = [];
+    }
+}
