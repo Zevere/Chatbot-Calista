b12c02158d10e89821b23e1be974dcae4dd90d86
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-07 19:13:49 -0500

    add task model

---
+++ src/borzoo/task.model.js
@@ -0,0 +1,70 @@
+// @flow
+/**
+ * A task item.
+ * @class Task
+ */
+export class Task {
+
+    /**
+     * The date the task was created on represented by a DateTimeOffset.
+     *
+     * __Must be an ISO 8601 datetime offset string.__
+     * 
+     * Example: 2018-11-07T22:59:50+00:00
+     * @see https://en.wikipedia.org/wiki/ISO_8601
+     * @type {string}
+     * @memberof Task
+     */
+    createdAt: string;
+
+    /**
+     * Description of this task.
+     * @type {string}
+     * @memberof Task
+     */
+    description: string;
+
+    /**
+     * Due date of this task.
+     * 
+     * __Must be an ISO 8601 date string.__
+     * 
+     * Example: 2018-05-28
+     * 
+     * @see https://en.wikipedia.org/wiki/ISO_8601
+     * @optional
+     * @type {string}
+     * @memberof Task
+     */
+    due: string;
+
+    /**
+     * The task's ID.
+     * @type {string}
+     * @memberof Task
+     */
+    id: string;
+
+    /**
+     * List of tags associated with this task.
+     * @type {string[]}
+     * @memberof Task
+     */
+    tags: string[];
+
+    /**
+     * Short title of this task.
+     * @type {string}
+     * @memberof Task
+     */
+    title: string;
+
+    constructor() {
+        this.createdAt = '';
+        this.description = '';
+        this.due = '';
+        this.id = '';
+        this.tags = [];
+        this.title = '';
+    }
+}
