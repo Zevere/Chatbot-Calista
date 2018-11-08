1d908c3100a72c594a0b706a7f6bf436b88620ce
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-07 22:24:02 -0500

    use apollo, wip addTask

--- src/borzoo/client.js
+++ src/borzoo/client.js
@@ -1,21 +1,45 @@
 // @flow
-import { axiosForBorzoo }  from '../config/axios';
-import { AxiosInstance } from 'axios';
+
 import { TaskInput } from './task-input.model';
 import { ListInput } from './list-input.model';
 import { UserInput } from './user-input.model';
 import { LoginInput } from './login-input.model';
 
+
+import ApolloClient, { gql } from 'apollo-boost';
+import { Task } from './task.model';
+
+
+
 export class Client {
-    axios: AxiosInstance;
+    client: ApolloClient;
 
     constructor() {
-        this.axios = axiosForBorzoo();
+        this.client = new ApolloClient({
+            uri: process.env.BORZOO_URL
+        });
     }
 
     
-    addTask(owner: string, list: string, task: TaskInput) { // eslint-disable-line no-unused-vars
-        throw Error('Unimplemented');
+    async addTask(owner: string, list: string, task: TaskInput): Promise<Task> {
+        return await this.client.mutate({
+            mutation: gql`
+                mutation {
+                    addTask(
+                        $owner,
+                        $list,
+                        $task
+                    ) {
+
+                    }
+                }
+            `,
+            variables: {
+                owner,
+                list,
+                task
+            }
+        });
     }
 
     createList(owner: string, list: ListInput) { // eslint-disable-line no-unused-vars
--- src/config/axios.js
+++ src/config/axios.js
@@ -12,12 +12,3 @@ export function axiosForVivid() {
     axios.defaults.headers['Content-Type'] = 'application/json';
     return axios;
 }
-
-export function axiosForBorzoo() {
-    const axios = Axios.create({
-        baseURL: process.env.BORZOO_URL,
-    });
-
-    axios.defaults.headers['Content-Type'] = 'application/json';
-    return axios;
-}
