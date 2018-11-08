51c3d09ad5a8559d37ea002ad51e3805a086e962
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-07 19:06:13 -0500

    add task input model

---
+++ src/borzoo/task-input.model.js
@@ -0,0 +1,61 @@
+// @flow
+/**
+ * Input model for creating a new task.
+ * @class TaskInput
+ */
+export class TaskInput {
+    
+    /**
+     * The desired task list name.
+     * Names are case insensitive and valid characters are ASCII 
+     * alphanumeric characters, '_', '.', and '-'.
+     * @type {string}
+     * @memberof TaskInput
+     */
+    id: string;
+    
+    /**
+     * Short title of the new task.
+     * @type {string}
+     * @memberof TaskInput
+     */
+    title: string;
+    
+    /**
+     * Description of the new task.
+     * @optional
+     * @type {string}
+     * @memberof TaskInput
+     */
+    description: string;
+
+    /**
+     * Date date of the new task.
+     * 
+     * __Must be an ISO 8601 date string.__
+     * 
+     * Example: 2018-05-28
+     * 
+     * @see https://en.wikipedia.org/wiki/ISO_8601
+     * @optional
+     * @type {string}
+     * @memberof TaskInput
+     */
+    due: string
+    
+    /**
+     * List of tags on the new task item.
+     * @optional
+     * @type {string[]}
+     * @memberof TaskInput
+     */
+    tags: string[];
+
+    constructor() {
+        this.id = '';
+        this.title = '';
+        this.description = '';
+        this.due = '';
+        this.tags = [];
+    }
+}
