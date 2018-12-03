67c3d716164302e116f5209652fe2d3dfb72e27d
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-12-03 00:18:35 -0500

    use zevere id

--- src/server/api/options/options.controller.js
+++ src/server/api/options/options.controller.js
@@ -3,14 +3,15 @@ import Winston from '../../../logging/app.logger';
 import { prettyJson } from '../../../logging/format';
 import { Client } from '../../../borzoo/client';
 import { Options } from './options.model';
-
+import { getUserBySlackId } from '../../authorization/authorization.service';
 
 export async function handleOptionsRequest(req: Request, res: Response, next: NextFunction) {
     const bz = new Client();
     req.body |> prettyJson |> Winston.debug;
     const opts: Options = req.body;
     try {
-        const lists = await bz.getLists(opts.user.id);
+        const user = await getUserBySlackId(opts.user.id); 
+        const lists = await bz.getLists(user.zevereId);
         const listOptions = lists.map(list => { return { label: list.title, value: list.id }; });
         res.status(200).send(listOptions);
     } catch (err) {
